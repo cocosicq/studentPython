cc13b9a28b5bcc5d30ab9cac51b3159c6ca4b846
cocosicq <32159510+cocosicq@users.noreply.github.com>
2017-09-21 15:08:34 +0300

    Update 3.3_homework.py

--- 3.3_homework.py
+++ 3.3_homework.py
@@ -2,7 +2,7 @@ import sys
 # -*- coding: utf-8 -*-
 weight = input('Weight: ')
 height = input('Height: ')
-
+height = height/100
 indexBody = float(weight) / (height*height)
 print('Your Weight index' + ' ' + str(indexBody))
 
